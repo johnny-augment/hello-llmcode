1433290030c5dc2c55a7dcd6d92e82a2117d7796
Johnny Augment <autonobotic.dev@gmail.com>
2023-09-18 11:59:31 -0400

    Fixed server_time

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+
+import datetime                                                                                                                
+                                                                                                                                  
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", server_time = datetime.datetime.now())
 if __name__ == '__main__':
     app.run()
